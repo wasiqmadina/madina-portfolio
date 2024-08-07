3604a9fd970a51c5ef64a3a173d303aaf9e9cf8f
Madina Wasiq <madina.wasiq2@gmail.com>
2024-08-07 17:40:53 -0400

    improved UI

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,33 +1,31 @@
+// src/components/ProjectCard/ProjectCard.jsx
 import React from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
-export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
-}) => {
+export const ProjectCard = ({ project }) => {
+  const { title, imageSrc, description, skills, demo, source } = project;
   return (
     <div className={styles.container}>
       <img
-        src={getImageUrl("projects/project.png")}
+        src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={styles.image}
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
+        {skills.map((skill, id) => (
+          <li key={id} className={styles.skill}>
+            {skill}
+          </li>
+        ))}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
+        <a href={demo} className={styles.link} >
           Demo
         </a>
-        <a href={source} className={styles.link}>
+        <a href={source} className={styles.link} >
           Source
         </a>
       </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,17 @@
+// src/components/Projects/Projects.jsx
 import React from "react";
 import styles from "./Projects.module.css";
 import projects from "../../data/projects.json";
-import { ProjectCard } from "./ProjectCard";
+import { ProjectCard } from "../Projects/ProjectCard";
 
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+        {projects.map((project, id) => (
+          <ProjectCard key={id} project={project} />
+        ))}
       </div>
     </section>
   );
